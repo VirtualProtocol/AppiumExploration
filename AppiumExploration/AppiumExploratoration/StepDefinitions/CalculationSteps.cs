046a124afeef5158b1060e90c4e3901f8a89ab0d
VirtualProtocol <adam.pankow@outlook.com>
2020-02-02 08:38:27 -0600

    added calculation steps to describe how to complete calculations

---
+++ AppiumExploration/AppiumExploratoration/StepDefinitions/CalculationSteps.cs
@@ -0,0 +1,31 @@
+﻿using AppiumExploratoration.ScreenObjects;
+using TechTalk.SpecFlow;
+
+namespace AppiumExploratoration.StepDefinitions
+{
+    [Binding]
+    public class CalculationSteps : StartUp
+    {
+        private static CalculatorScreen _calculatorScreen { get; set; }
+
+
+        [Before]
+        public void BeforeScenario()
+        {
+            _calculatorScreen = new CalculatorScreen(MobileDriver);
+            _calculatorScreen.ClearInputField();
+        }
+
+        [When(@"I press the add button")]
+        public void WhenIPressTheAddButton()
+        {
+            _calculatorScreen.ClickAdd();
+        }
+        
+        [When(@"I press the calculate button")]
+        public void WhenIPressTheCalculateButton()
+        {
+            _calculatorScreen.Calculate();
+        }
+    }
+}
