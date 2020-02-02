66ee0af38579bcda60e4a72a5c3e9cf88afae112
VirtualProtocol <adam.pankow@outlook.com>
2020-02-02 08:08:30 -0600

    added step definitions file with number step definitions

---
+++ AppiumExploration/AppiumExploratoration/StepDefinitions/NumbersSteps.cs
@@ -0,0 +1,32 @@
+﻿using AppiumExploratoration.ScreenObjects;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace AppiumExploratoration.StepDefinitions
+{
+    [Binding]
+    public class NumbersSteps : StartUp
+    {
+        private static CalculatorScreen _calculatorScreen { get; set; }
+
+
+        [Before]
+        public void BeforeScenario()
+        {
+            _calculatorScreen = new CalculatorScreen(MobileDriver);
+            _calculatorScreen.ClearInputField();
+        }
+
+        [When(@"I enter (.*) into the calculator")]
+        public void WhenIEnterIntoTheCalculator(string number)
+        {
+            _calculatorScreen.EnterNumber(number);
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(string number)
+        {
+            Assert.AreEqual(number, _calculatorScreen.PrintInputFieldText());
+        }
+    }
+}
